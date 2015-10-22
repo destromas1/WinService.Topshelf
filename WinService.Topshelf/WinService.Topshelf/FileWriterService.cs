7e2d3b923cf2c1a1c46b66055bdc0cdc5784a616
Shahjada <shahjada.talukdar@gmail.com>
2015-10-23 01:22:56 +0600

    TopShelf for Windows Service

---
+++ WinService.Topshelf/WinService.Topshelf/FileWriterService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WinService.Topshelf
+{
+    class FileWriterService : ServiceControl
+    {
+        public bool Start(HostControl hostControl)
+        {
+            File.WriteAllText("info.txt", "Start");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            File.WriteAllText("info.txt", "Stop");
+            return true;
+        }
+    }
+}
---
+++ WinService.Topshelf/WinService.Topshelf/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace WinService.Topshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<FileWriterService>();
+                x.RunAsLocalSystem();
+                x.SetDescription("FileWriterService");
+                x.SetDisplayName("FileWriterService");
+                x.SetServiceName("FileWriterService");
+                x.StartAutomatically();
+                x.EnableServiceRecovery(rc => rc.RestartService(1));
+
+            });
+        }
+    }
+}
